42a4eb697dab2bdcb9c9bbb67a9450f62b439145
Ahsokastar <116227223+Ahsokastar@users.noreply.github.com>
2025-01-05 22:51:31 -0700

    Update trial1.py

--- trial1.py
+++ trial1.py
@@ -1,6 +1,6 @@
 import folium
-import os
 import pandas as pd 
+import os
 import requests
 from folium.plugins import HeatMap
 from kaggle.api.kaggle_api_extended import KaggleApi
@@ -21,7 +21,7 @@ else:
 def load_data():
     api = KaggleApi()
     api.authenticate()
-    dataset_identifier = 'anoopjohny/traffic-crashes-crashes'  
+    dataset_identifier = 'catherinetodd123/traffic-crash-chicago-api-address'  
     DATA_DIR = 'data'
     os.makedirs(DATA_DIR, exist_ok=True)
 
@@ -29,7 +29,7 @@ def load_data():
     api.dataset_download_files(dataset_identifier, path=DATA_DIR, unzip=True)
 
     
-    crash_path = os.path.join(DATA_DIR, 'Traffic_Crashes_-_Crashes.csv')
+    crash_path = os.path.join(DATA_DIR, 'red-light-camera-violations.csv')
 
    
     crash_pd = pd.read_csv(crash_path)
@@ -43,22 +43,7 @@ crash_data = load_data()
 
 crash_data['CRASH_DATE'] = pd.to_datetime(crash_data['CRASH_DATE'], format='%m/%d/%Y %I:%M:%S %p')  
 
-for idx, row in crash_data.iterrows():
-    if pd.isnull(row['LATITUDE']):
-        url = "https://geocoding.geo.census.gov/geocoder/locations/onelineaddress?address={num}+{street_name},+Chicago,+IL&benchmark=Public_AR_Current&format=json"
-        #strname_plus = row['STREET_DIRECTION'] + '+' + '+'.join(row['STREET_NAME'].split(" ")).lower()
-        try:    
-            strname_plus = row['STREET_DIRECTION'] + '+' + '+'.join(row['STREET_NAME'].split(" ")).lower()
-            response = requests.get(url.format(num=row['STREET_NO'], street_name=strname_plus))
-            print(url.format(num=row['STREET_NO'], street_name=strname_plus))
-            query_result = response.json()
-            lat = query_result["result"]["addressMatches"][0]["coordinates"]["x"]
-            long = query_result["result"]["addressMatches"][0]["coordinates"]["y"]
-            crash_data.at[idx, 'LATITUDE'] = lat
-            crash_data.at[idx, 'LONGITUDE'] = long
-            
-        except:
-            pass
+
 
 years = crash_data['CRASH_DATE'].dt.year.unique()
 m = folium.Map(location=(41.8781, -87.6298), tiles = 'https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', attr = 'mine')
@@ -72,8 +57,6 @@ for year in years:
 
 folium.LayerControl(collapsed=False).add_to(m)
 folium_static(m, width = 1400, height = 1000)
-#HeatMap(lat_long_intensity, radius = 7, blur = .5).add_to(m)
-#m.save("index.html")
 
 
 
